27263ad30a75cd534fdcf7ac33ca2551761f81e6
dali <lionelviperdali@gmail.com>
2021-06-26 13:11:10 +0100

    #1 * create BikeRepository to get data from endpoint

---
+++ lib/core/repository/BikeRepository.dart
@@ -0,0 +1,30 @@
+import 'package:bikes_shop/core/repository/base_repository.dart';
+import 'package:bikes_shop/domain/models/response.dart';
+import 'package:bikes_shop/domain/repository/i_bike_repository.dart';
+
+class BikeRepository with BaseRepository implements IBikeRepository {
+  @override
+  Future<IResponse> getAll() async {
+    var response = await get(endpoint: "bikes/all");
+
+    return BikesResponse(response.data as List<Map<String, dynamic>>);
+  }
+
+  @override
+  Future<IResponse> getAllByFilter(filter) {
+    // TODO: implement getAllByFilter
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<IResponse> getDetailBike(int idBike) {
+    // TODO: implement getDetailBike
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<IResponse> getOne(int id) {
+    // TODO: implement getOne
+    throw UnimplementedError();
+  }
+}
