565c560aa54af841745b9f319d2b3ea78d835033
dali <lionelviperdali@gmail.com>
2021-06-28 19:32:29 +0100

    #4 * add bike parameter for DetailBike

--- lib/app/ui/pages/detail_bike.dart
+++ lib/app/ui/pages/detail_bike.dart
@@ -1,6 +1,14 @@
+import 'package:bikes_shop/domain/models/bike.dart';
 import 'package:flutter/material.dart';
 
 class DetailBike extends StatelessWidget {
+  final Bike bike;
+
+  const DetailBike({
+    Key? key,
+    required this.bike,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container();
