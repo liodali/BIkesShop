55bcf3ab71a0c015ae4fecbcd45a823f037b63d8
dali <lionelviperdali@gmail.com>
2021-06-29 01:43:06 +0100

    #1 * build runner output

--- lib/app/common/locator.config.dart
+++ lib/app/common/locator.config.dart
@@ -8,6 +8,7 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../../core/interactor/get_all_bikes_use_case.dart' as _i5;
+import '../../core/interactor/get_detail_bike_use_case.dart' as _i6;
 import '../../core/repository/BikeRepository.dart' as _i4;
 import '../../domain/repository/i_bike_repository.dart'
     as _i3; // ignore_for_file: unnecessary_lambdas
@@ -20,5 +21,7 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   gh.lazySingleton<_i3.IBikeRepository>(() => _i4.BikeRepository());
   gh.factory<_i5.GetAllBikesUseCase>(
       () => _i5.GetAllBikesUseCase(get<_i3.IBikeRepository>()));
+  gh.factory<_i6.GetDetailBikeUseCase>(
+      () => _i6.GetDetailBikeUseCase(get<_i3.IBikeRepository>()));
   return get;
 }
